0e5a31622b1133f19887d6779533b8fc0d455845
zhangqipu <qipu.zhang@qipu.local>
2016-02-04 20:04:54 +0800

    commit all

---
+++ Assets/BullectMove.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BullectMove : MonoBehaviour {
+
+	private int longLife = 360;
+	public int ID = 100;
+
+	// Use this for initialization
+	void Start () {
+		longLife = 360;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		if (longLife == 0 && ID == 0) {
+			Destroy (this.gameObject);
+			Debug.Log("Die");
+			return ;
+		}
+
+		this.transform.Translate (Vector3.up * 0.1f);
+
+		longLife--;
+	}
+	
+}
---
+++ Assets/Main.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+	public GameObject tank;
+	public GameObject bulletPrefab;
+
+	public float speed = 0.2f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+		if (Input.GetKey (KeyCode.D)) {
+			tank.transform.Translate(Vector3.right  * (speed));
+		}
+
+		if (Input.GetKey (KeyCode.A)) {
+			tank.transform.Translate(Vector3.left  * (speed));
+		}
+
+		if (Input.GetKey (KeyCode.W)) {
+			tank.transform.Translate(Vector3.up * (speed));
+		}
+
+		if (Input.GetKey (KeyCode.S)) {
+			tank.transform.Translate(Vector3.down * (speed));
+		}
+
+		if (Input.GetKeyDown (KeyCode.Space)) {
+			GameObject bullet = (GameObject)Instantiate(bulletPrefab);
+			bullet.GetComponent<BullectMove>().ID = 0;
+			bullet.GetComponent<Transform>().position = tank.transform.position;
+		}
+	}
+}
